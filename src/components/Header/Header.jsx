ffd849121ca56eed9ca7c500b0834df72a297365
omghadia <103301669+omghadia@users.noreply.github.com>
2024-10-08 19:03:47 +0530

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import { Logo } from "../Logo/Logo";
+import s from "./style.module.css";
+
+export function Header() {
+  return (
+    <>
+      <Logo title="Web App Firewall" subtitle="Protect your website" />
+    </>
+  );
+}
